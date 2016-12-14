4530640d63ab49020ba68982828f7380e682ffe2
Fred Cons <cons@fullsix.com>
2016-12-14 14:55:01 +0100

    Attempting automatic SAM deployment

---
+++ lambda_function.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import boto3
+import json
+
+print('Loading function')
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
+    access to the request and response payload, including headers and
+    status code.
+
+    To scan a DynamoDB table, make a GET request with the TableName as a
+    query string parameter. To put, update, or delete an item, make a POST,
+    PUT, or DELETE request respectively, passing in the payload to the
+    DynamoDB API as a JSON body.
+    '''
+    #print("Received event: " + json.dumps(event, indent=2))
+    return respond(None, event))
+
